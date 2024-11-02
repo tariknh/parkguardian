4f99b13c831468a09ad520f498be70be94b43cbd
Tarik <hajibtarik@gmail.com>
2024-11-03 00:51:34 +0100

    auth update

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoApi.Dtos.Account;
 using TodoApi.Interfaces;
 using TodoApi.Mappers;
@@ -13,11 +14,36 @@ public class AccountController : ControllerBase
 {
 private readonly UserManager<AppUser> _userManager;
 private readonly ITokenService _tokenService;
-public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+private readonly SignInManager<AppUser> _signinManager;
+public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signinManager)
 {
     _userManager = userManager;
     _tokenService = tokenService;
+    _signinManager = signinManager;
 }
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDto loginDto)
+    {
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+        if (user == null) return Unauthorized("Invalid Username!");
+
+        var result = await _signinManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if(!result.Succeeded) return Unauthorized("Username not found and/or password incorrect");
+
+        return Ok(
+            new NewUserDto{
+                UserName = user.UserName,
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)
+            }
+        );
+
+    }
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
     {
--- Dtos/Account/NewUserDto.cs
+++ Dtos/Account/NewUserDto.cs
@@ -5,7 +5,7 @@ namespace TodoApi.Dtos.Account;
 public class NewUserDto
 {
     public string? Email { get; set; }
-    public string? Username { get; set; }
+    public string? UserName { get; set; }
     public string? Token { get; set; }
 
 }
